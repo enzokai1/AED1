276b3bff87a3466a71975287c308278ab08d6ff0
enzokai1 <enzo.massahiro.kai@gmail.com>
2026-01-08 12:09:17 -0300

    Create 1298.c

--- 1298.c
+++ 1298.c
@@ -1,96 +1,124 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef struct {
-    int u, v, w;
-} Aresta;
 
-Aresta arestas[400005];
-int pai[400005];
-int N;
-int num_arestas;
+struct Aresta {
+    int origem;
+    int destino;
+    int peso; 
+};
 
-int buscar(int i) {
-    if (pai[i] == i)
-        return i;
-    return pai[i] = buscar(pai[i]);
+
+struct Aresta lista_arestas[400005];
+int chefe_grupo[400005]; 
+int contador_arestas;
+int tamanho_N;
+
+//transformando a coordenada em um numero so 
+int calcular_indice(int linha, int coluna) {
+    int largura = 2 * tamanho_N + 2;
+    return linha * largura + coluna;
 }
 
-void unir(int i, int j) {
-    int raiz_i = buscar(i);
-    int raiz_j = buscar(j);
-    if (raiz_i != raiz_j) {
-        pai[raiz_i] = raiz_j;
+// union-find
+int encontrar_chefe(int i) {
+    if (chefe_grupo[i] == i) return i;
+    return chefe_grupo[i] = encontrar_chefe(chefe_grupo[i]);
+}
+
+
+void unir_conjuntos(int i, int j) {
+    int chefe_i = encontrar_chefe(i);
+    int chefe_j = encontrar_chefe(j);
+    if (chefe_i != chefe_j) {
+        chefe_grupo[chefe_i] = chefe_j;
     }
 }
 
-int comparar(const void* a, const void* b) {
-    return ((Aresta*)a)->w - ((Aresta*)b)->w;
-}
-
-int get_id(int r, int c) {
-    return r * (2 * N + 2) + c;
-}
-
-void adicionar_aresta(int u, int v, int w) {
-    arestas[num_arestas].u = u;
-    arestas[num_arestas].v = v;
-    arestas[num_arestas].w = w;
-    num_arestas++;
+void criar_aresta(int u, int v, int w) {
+    lista_arestas[contador_arestas].origem = u;
+    lista_arestas[contador_arestas].destino = v;
+    lista_arestas[contador_arestas].peso = w;
+    contador_arestas++;
 }
 
 int main() {
-    char buffer[605];
+    char buffer_leitura[605];
     
-    while (scanf("%d", &N) != EOF) {
-        num_arestas = 0;
-        int linhas = 2 * N;
-        int colunas = 2 * N + 1;
-        int total_nos = (linhas + 1) * (colunas + 1);
+    while (scanf("%d", &tamanho_N) != EOF) {
+        contador_arestas = 0;
+        
+        int total_linhas = 2 * tamanho_N;
+        int total_colunas = 2 * tamanho_N + 1;
+        int max_nos = (total_linhas + 1) * (total_colunas + 1);
 
-        for (int i = 0; i <= total_nos; i++) {
-            pai[i] = i;
+        // union-find
+        for (int i = 0; i <= max_nos; i++) {
+            chefe_grupo[i] = i;
         }
 
-        int raiz_borda = get_id(0, 0);
-        for (int c = 0; c <= colunas; c++) {
-            unir(raiz_borda, get_id(0, c));
-            unir(raiz_borda, get_id(linhas, c));
+        // conectando as bordas
+        int raiz_borda = calcular_indice(0, 0);
+        
+        for (int c = 0; c <= total_colunas; c++) {
+            unir_conjuntos(raiz_borda, calcular_indice(0, c));
+            unir_conjuntos(raiz_borda, calcular_indice(total_linhas, c));
         }
-        for (int r = 0; r <= linhas; r++) {
-            unir(raiz_borda, get_id(r, 0));
-            unir(raiz_borda, get_id(r, colunas));
+        for (int r = 0; r <= total_linhas; r++) {
+            unir_conjuntos(raiz_borda, calcular_indice(r, 0));
+            unir_conjuntos(raiz_borda, calcular_indice(r, total_colunas));
         }
 
-        for (int i = 1; i < 2 * N; i++) {
-            scanf("%s", buffer);
-            for (int j = 0; j < N; j++) {
+        // criando arestas
+        for (int i = 1; i < 2 * tamanho_N; i++) {
+            scanf("%s", buffer_leitura);
+            for (int j = 0; j < tamanho_N; j++) {
                 int r = i;
                 int c = (i % 2 != 0) ? (2 * j + 1) : (2 * j + 2);
                 
-                int u_h = get_id(r, c - 1);
-                int v_h = get_id(r, c + 1);
-                int w_h = (buffer[j] == 'H') ? 0 : 1;
-                adicionar_aresta(u_h, v_h, w_h);
+                // horizontal
+                int u_h = calcular_indice(r, c - 1);
+                int v_h = calcular_indice(r, c + 1);
+                int peso_h = (buffer_leitura[j] == 'H') ? 0 : 1;
+                criar_aresta(u_h, v_h, peso_h);
 
-                int u_v = get_id(r - 1, c);
-                int v_v = get_id(r + 1, c);
-                int w_v = (buffer[j] == 'V') ? 0 : 1;
-                adicionar_aresta(u_v, v_v, w_v);
+                // vertical
+                int u_v = calcular_indice(r - 1, c);
+                int v_v = calcular_indice(r + 1, c);
+                int peso_v = (buffer_leitura[j] == 'V') ? 0 : 1;
+                criar_aresta(u_v, v_v, peso_v);
             }
         }
 
-        qsort(arestas, num_arestas, sizeof(Aresta), comparar);
+       // kruskal sem ordenacao
+        int custo_total = 0;
 
-        int trocas = 0;
-        for (int i = 0; i < num_arestas; i++) {
-            if (buscar(arestas[i].u) != buscar(arestas[i].v)) {
-                unir(arestas[i].u, arestas[i].v);
-                trocas += arestas[i].w;
+        // prioridade maxima
+        for (int i = 0; i < contador_arestas; i++) {
+            if (lista_arestas[i].peso == 0) {
+                int u = lista_arestas[i].origem;
+                int v = lista_arestas[i].destino;
+                
+                if (encontrar_chefe(u) != encontrar_chefe(v)) {
+                    unir_conjuntos(u, v);
+                    
+                }
             }
         }
 
-        printf("%d\n", trocas);
+        // prioridade baixa
+        for (int i = 0; i < contador_arestas; i++) {
+            if (lista_arestas[i].peso == 1) {
+                int u = lista_arestas[i].origem;
+                int v = lista_arestas[i].destino;
+                
+                if (encontrar_chefe(u) != encontrar_chefe(v)) {
+                    unir_conjuntos(u, v);
+                    custo_total += 1;
+                }
+            }
+        }
+
+        printf("%d\n", custo_total);
     }
     return 0;
 }

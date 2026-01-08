f609a0c2bab0b5475c7810d3a8605774049aaafa
enzokai1 <enzo.massahiro.kai@gmail.com>
2026-01-08 13:15:12 -0300

    Create 1068a.c

--- 1068a.c
+++ 1068a.c
@@ -1,57 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-#define MAX_TAM 1001 
 
-char pilha[MAX_TAM]; 
-int topo;            
+typedef struct {
+    char itens[1005];
+    int topo;
+} Pilha;
 
-void inicializarPilha() {
-    topo = -1;
+
+void inicializar(Pilha *p) {
+    p->topo = -1;
 }
 
-int estaVazia() {
-    return (topo == -1);
+
+int esta_vazia(Pilha *p) {
+    if (p->topo == -1) {
+        return 1; 
+    }
+    return 0; 
 }
 
-void empilhar(char c) {
-    topo++;
-    pilha[topo] = c;
+
+void empilhar(Pilha *p, char caractere) {
+   
+    p->topo++;
+    p->itens[p->topo] = caractere;
 }
 
-void desempilhar() {
-    topo--;
+
+void desempilhar(Pilha *p) {
+    if (esta_vazia(p) == 0) {
+        p->topo--;
+    }
 }
 
-int verificarParenteses(char* expressao) {
-    inicializarPilha();
 
+int verificar_parenteses(char *expressao) {
+    Pilha p;
+    inicializar(&p); // endereco da pilha
+
+    
     for (int i = 0; expressao[i] != '\0'; i++) {
-        
-        if (expressao[i] == '(') {
-            empilhar('(');
+        char atual = expressao[i];
+
+        if (atual == '(') {
+            empilhar(&p, '(');
+        }
+        else if (atual == ')') {
             
-        } else if (expressao[i] == ')') {
-            
-            if (estaVazia()) {
+            if (esta_vazia(&p)) {
                 return 0; 
-            } else {
-                desempilhar();
             }
+            // se tem par, remove da pilha
+            desempilhar(&p);
         }
     }
 
-    return estaVazia();
+    // no final, a pilha deve estar vazia para ser correto
+    return esta_vazia(&p);
 }
 
 int main() {
-    char expressao[MAX_TAM + 2]; 
+    char entrada[1005];
 
-    while (fgets(expressao, sizeof(expressao), stdin) != NULL) {
+    
+    while (fgets(entrada, 1005, stdin) != NULL) {
         
-        expressao[strcspn(expressao, "\n")] = '\0';
-
-        if (verificarParenteses(expressao)) {
+        
+        if (verificar_parenteses(entrada)) {
             printf("correct\n");
         } else {
             printf("incorrect\n");

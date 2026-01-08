56e978d54af1bd752c3bed709db5c6ae871d9c15
enzokai1 <enzo.massahiro.kai@gmail.com>
2026-01-08 12:22:06 -0300

    Create 1197.c

--- 1197.c
+++ 1197.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int v, t;
+    int velocidade, tempo;
 
-    while (scanf("%d %d", &v, &t) != EOF) {
-        printf("%d\n", v * t * 2);
+    while (scanf("%d %d", &velocidade, &tempo) == 2) {
+        
+        int deslocamento = velocidade * tempo * 2;
+        
+        printf("%d\n", deslocamento);
     }
 
     return 0;

7cadcc6b9cc8516e6615f8f6950ed06c05ad98e4
enzokai1 <enzo.massahiro.kai@gmail.com>
2026-01-08 11:01:54 -0300

    Create 1583.c

--- 1583.c
+++ 1583.c
@@ -1,44 +1,59 @@
 #include <stdio.h>
 
-int N, M;
-char mapa[55][55];
 
-void dfs(int r, int c) {
-    int dr[] = {-1, 1, 0, 0};
-    int dc[] = {0, 0, -1, 1};
+char mapa[60][60];
+int linhas, colunas;
 
-    mapa[r][c] = 'T';
+// dfs
+void espalhar_tinta(int l, int c) {
+    
+    // 0 = cima, 1 = baixo, 2 = esquerda, 3 = direita
+    int mov_linha[] = {-1, 1, 0, 0};
+    int mov_coluna[] = {0, 0, -1, 1};
 
+    //posicao atual
+    mapa[l][c] = 'T';
+
+    
     for (int i = 0; i < 4; i++) {
-        int nr = r + dr[i];
-        int nc = c + dc[i];
+        int nova_l = l + mov_linha[i];
+        int nova_c = c + mov_coluna[i];
 
-        if (nr >= 0 && nr < N && nc >= 0 && nc < M) {
-            if (mapa[nr][nc] == 'A') {
-                dfs(nr, nc);
+        //ve se a nova posicao ta tendo ta matriz
+        if (nova_l >= 0 && nova_l < linhas && nova_c >= 0 && nova_c < colunas) {
+            
+            // se o vizinho for A, chama a recursao
+            if (mapa[nova_l][nova_c] == 'A') {
+                espalhar_tinta(nova_l, nova_c);
             }
         }
     }
 }
 
 int main() {
-    while (scanf("%d %d", &N, &M) && (N != 0 || M != 0)) {
-        for (int i = 0; i < N; i++) {
+    
+    while (scanf("%d %d", &linhas, &colunas) && (linhas != 0 || colunas != 0)) {
+        
+        
+        for (int i = 0; i < linhas; i++) {
             scanf("%s", mapa[i]);
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+        
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
                 if (mapa[i][j] == 'T') {
-                    dfs(i, j);
+                    espalhar_tinta(i, j);
                 }
             }
         }
 
-        for (int i = 0; i < N; i++) {
+        
+        for (int i = 0; i < linhas; i++) {
             printf("%s\n", mapa[i]);
         }
-        printf("\n");
+        printf("\n"); 
     }
+
     return 0;
 }
